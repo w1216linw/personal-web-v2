3b6e13f19289c87d63b952e9c089939343bb9866
Weiwei <w1216lin@gmail.com>
2023-06-14 01:46:32 -0500

    fix: text overflow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,14 @@ const Home = ({ homeRef }) => {
       ref={homeRef}
       className="grid place-content-center h-screen relative"
     >
-      <div className="max-w-2xl text-center space-y-6 ">
-        <h1 className="text-6xl font-bold flex gap-4 justify-center">
+      <div className="max-w-2xl text-center space-y-6">
+        <h1 className="md:text-6xl sm:text-5xl text-3xl font-bold flex gap-4 justify-center">
           <p className="animate-[jump_1s_0ms_1]">Hi,</p>
           <p className="animate-[jump_1s_500ms_1]">I&apos;m</p>
           <p className="animate-[jump_1s_1000ms_1]">Weiwei</p>
           <p className="animate-[jump_1s_1500ms_1]">Lin.</p>
         </h1>
-        <p className="text-xl text-[--clr-primary-2]">
+        <p className="md:text-xl text-base text-[--clr-primary-2]">
           A Frontend engineer that passionate about building user-centric
           interaction interface.
         </p>
